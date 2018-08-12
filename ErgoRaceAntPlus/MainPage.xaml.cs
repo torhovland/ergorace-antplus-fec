528fd472bff6cba93f0467e00c8cde26bb710fc1
Tor Hovland <tor+gitextensions@hovland.co>
2018-08-12 10:15:35 +0200

    Getting serial communication working.

--- ErgoRaceAntPlus/MainPage.xaml.cs
+++ ErgoRaceAntPlus/MainPage.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,13 +29,53 @@ namespace ErgoRaceAntPlus
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        readonly List<DeviceInformation> comPorts = new List<DeviceInformation>();
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void TopLevelNav_OnLoaded(object sender, RoutedEventArgs e)
+        private async void TopLevelNav_OnLoaded(object sender, RoutedEventArgs e)
         {
+            foreach (var port in await DeviceInformation.FindAllAsync(SerialDevice.GetDeviceSelector()))
+            {
+                comPorts.Add(port);
+
+                using (var serialPort = await SerialDevice.FromIdAsync(port.Id))
+                using (var inputStream = serialPort.InputStream)
+                using (var outputStream = serialPort.OutputStream)
+                using (var reader = new DataReader(inputStream))
+                using (var writer = new DataWriter(outputStream))
+                {
+                    serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+                    serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+                    serialPort.BaudRate = 9600;
+                    serialPort.Parity = SerialParity.None;
+                    serialPort.StopBits = SerialStopBitCount.One;
+                    serialPort.DataBits = 8;
+                    serialPort.Handshake = SerialHandshake.None;
+
+                    writer.WriteString("VE\r\n");
+                    await writer.StoreAsync();
+
+                    reader.InputStreamOptions = InputStreamOptions.Partial;
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        var length = await reader.LoadAsync(2048);
+
+                        if (length > 0)
+                        {
+                            var s = reader.ReadString(length);
+                            var t = s;
+                        }
+
+                        await Task.Delay(500);
+                    }
+                }
+            }
+
             TopLevelNav.SelectedItem = TopLevelNav.MenuItems[0];
             ContentFrame.Navigate(typeof(DashboardPage));
         }
--- ErgoRaceAntPlus/SettingsPage.xaml.cs
+++ ErgoRaceAntPlus/SettingsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
